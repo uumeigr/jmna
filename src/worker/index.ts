0b4aef1e1b185c9d4482bc310ade953c70dc73ea
uumeigr <jade.android.2011@gmail.com>
2026-02-20 11:29:08 +0800

    Update index.ts

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,31 @@
 import { Hono } from "hono";
 const app = new Hono<{ Bindings: Env }>();
 
+import { Context } from "hono";
+//import xxxApi from 'xxxApiPackage';
+
+const xxxApiCaller = async function(c: Context) {
+
+  // ... call the xxxApi
+  // ... check if the calling is successful
+  // ... get the result of the xxxApi calling
+  const isSuccess = true;       // replace it with the real api call result in
+  const resStr = 'hahahaha';     // replace it with the real api call result
+  
+  if (isSuccess) {
+      console.log(resStr);
+      return c.json({ resString: resStr });
+    
+  } else{
+    return c.json({ resString: "https://www.google.com" });
+  }
+  
+}
+
+
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+
+app.get("/api/xxxapi", (c) => xxxApiCaller(c));
+
 
 export default app;
